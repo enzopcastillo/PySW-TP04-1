0d023036d4e9023e93460891a8c7a4ffa87c7b78
enzopcastillo <epcastillov@gmail.com>
2022-05-14 16:11:28 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CastilloEnzo-LU3532-TPN4-Parte1';
+}
